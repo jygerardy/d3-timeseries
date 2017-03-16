a7b84563f738775dd0b6d5e2a6e472b4fd7964c2
jygerardy <jeanyvesgerardy@gmail.com>
2017-03-16 10:28:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+    //dynamically populate dropdowns
+    var newSelectDate = document.createElement('select'),
+        newSelectSeries = document.createElement('select'),
+        selectDateHTML = "",
+        selectSeriesHTML = "";
+
+    newSelectDate.className = "form-control";
+    newSelectSeries.className = "form-control";
+    newSelectDate.id = "chooseDate";
+    newSelectSeries.id = "chooseSeries";
+
+    $.getJSON("/columns", function(data){
+
+        var columns =  data.columns;
+            timeseries = data.timeseries,
+            columnsLen = columns.length,
+            timeseriesLen = timeseries.length
+
+        for(i = 0; i < columnsLen; i++){
+            selectDateHTML += "<option value='" + columns[i] + "'>" + columns[i]+"</option>";
+        };
+
+        for(i = 0; i < timeseriesLen; i++){
+            selectSeriesHTML += "<option value='" + timeseries[i] + "'>" + timeseries[i]+"</option>";
+        };
+        newSelectSeries.innerHTML = selectSeriesHTML;
+        newSelectDate.innerHTML = selectDateHTML;
+        document.getElementById('chooseSeriesDiv').appendChild(newSelectSeries);
+        document.getElementById('chooseDateDiv').appendChild(newSelectDate);
+    });
+
+});
+
+function buttonVizPressed() {
+
+
+  
+  var date_col = document.getElementById("chooseDate").value,
+      series = document.getElementById("chooseSeries").value,
+      periods = document.getElementById("choosePeriods").value
+
+  var params = {"series": series, "date_col": date_col, "periods": periods}
+
+  $.get("/graph", params, function(d){
+      var d_array = JSON.parse(d);
+      var new_data = d_array.map(function(arg) {
+        arg["ds"] = new Date(arg["ds"]);
+        return arg;
+      });
+
+      var chart = d3.timeseries()
+                .addSerie(new_data,
+                  {x : "ds",
+                   y : "yhat",
+                   diff:'n3'},
+                   {interpolate:'monotone',color:"#333"})
+                .addSerie(new_data,
+                  {x : "ds",
+                   y : "yhat_lower",
+                   diff:'n1'},
+                   {interpolate:'monotone',color:"red"})
+                .addSerie(new_data,
+                  {x : "ds",
+                   y : "yhat_upper",
+                   diff:'n1'},
+                   {interpolate:'monotone',color:"red"})
+                .width(900);
+
+
+      $('#graph-container').empty();
+      chart('#graph-container');
+
+  })
+};
